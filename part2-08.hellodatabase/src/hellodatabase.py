5dd4069e1afa6093df698828939355eee768fd5a
stevelaurensius <steve.laurensius+github@gmail.com>
2024-10-06 16:25:14 +0700

    Initial Commit

---
+++ part2-08.hellodatabase/src/hellodatabase.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+import sqlite3
+
+
+def read_database(conn):
+	agents = []
+
+	# output should be a list of pairs
+	# agents = [(id1, name1), (id2, name2), (id3, name3), ...] ordered by id
+
+
+	# write code here
+	statement = 'SELECT id, name FROM Agent'
+	agents = conn.execute(statement).fetchall()
+	agents.sort()
+
+	return agents
+
+
+
+def main(argv):
+	name = sys.argv[1]
+	conn = sqlite3.connect(name)
+	agents = read_database(conn)
+	for agent in agents:
+		print(agent[0], agent[1])
+
+# This makes sure the main function is not called immediatedly
+# when TMC imports this module
+if __name__ == "__main__": 
+	if len(sys.argv) != 2:
+		print('usage: python %s database' % sys.argv[0])
+	else:
+		main(sys.argv)
